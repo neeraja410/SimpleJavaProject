ae66f2da99f3ec04cb3041a5fa3ffe541a0f1c3d
neeraja410 <mudiredddy95@gmail.com>
2024-08-05 17:44:07 -0700

    Added code of adding  two numbers  to helloworld program

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class HelloWorld {
     public static void main(String[] args) {
+       int a,b,sum;
+       sum=a+b;
+       System.out.println(sum);
         System.out.println("Hello, World!");
         System.out.println("from testbranch");
        //added some more print satements
